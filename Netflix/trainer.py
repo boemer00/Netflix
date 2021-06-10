208ef62c7e5a4017bbeebc10da725e556618edc9
Priscilla Fearn <fearn.priscilla@gmail.com>
2021-06-10 14:59:42 +0100

    trainer.py updates

--- Netflix/trainer.py
+++ Netflix/trainer.py
@@ -1,3 +1,26 @@
+import requests
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+import math
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from data import DataSourcing
+import joblib
+from termcolor import colored
+from memoized_property import memoized_property
+from mlflow.tracking import MlflowClient 
+
+
+
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+EXPERIMENT_NAME = "Netflix_experiment"
+
 class Trainer(object):
     def __init__(self, X, y):
         """
@@ -5,7 +28,9 @@ class Trainer(object):
             y: pandas Series
         """
         self.pipeline = None
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size = 0.3)
+        #self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size = 0.3)
+        self.X = X
+        self.y = y
         
     def set_pipeline(self):
         """defines the pipeline as a class attribute"""
@@ -30,16 +55,16 @@ class Trainer(object):
                 ('linear_model', LinearRegression())
             ])
     def fit_pipeline(self):
-        self.pipeline = self.pipeline.fit(self.X_train, self.y_train)
+        self.pipeline = self.pipeline.fit(self.X, self.y)
         print('pipeline fitted')
         
-    def evaluate(self):
-        y_pred_train = self.pipeline.predict(self.X_train)
-        mse_train = mean_squared_error(self.y_train, y_pred_train)
+    def evaluate(self, X_test, y_test):
+        y_pred_train = self.pipeline.predict(self.X)
+        mse_train = mean_squared_error(self.y, y_pred_train)
         rmse_train = np.sqrt(mse_train)
         
-        y_pred_test = self.pipeline.predict(self.X_test)
-        mse_test = mean_squared_error(self.y_test, y_pred_test)
+        y_pred_test = self.pipeline.predict(X_test)
+        mse_test = mean_squared_error(y_test, y_pred_test)
         rmse_test = np.sqrt(mse_test)
         return (round(rmse_train, 3) ,round(rmse_test, 3))
         
@@ -47,20 +72,51 @@ class Trainer(object):
         y_pred = self.pipeline.predict(X)
         return y_pred
    
+    def save_model(self):
+        """Save the model into a .joblib format"""
+        joblib.dump(self.pipeline, 'model.joblib')
+        print(colored("model.joblib saved locally", "green"))
+
+ # MLFlow methods
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+            return self.mlflow_client.get_experiment_by_name(
+                self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
-# if __name__ == "__main__":
-#     # Get and clean data
-#     N = 10000
-#     df = get_data(nrows=N)
-#     df = clean_data(df)
-#     y = df["fare_amount"]
-#     X = df.drop("fare_amount", axis=1)
-#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-#     # Train and save model, locally and
-#     trainer = Trainer(X=X_train, y=y_train)
-#     trainer.set_experiment_name('xp2')
-#     trainer.run()
-#     rmse = trainer.evaluate(X_test, y_test)
-#     print(f"rmse: {rmse}")
-#     trainer.save_model()
+
+
+if __name__ == "__main__":
+    # Get and clean data
+    N = 1000
+    df = DataSourcing()
+    df = df.load_data(N)
+    df = df.data_wrangling(df)
+    y = y
+    X = X
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    # Train and save model, locally and
+    trainer = Trainer(X_train, y_train)
+    trainer.set_experiment_name('NetflixTeam')
+    trainer.run()
+    rmse = trainer.evaluate(X_test, y_test)
+    print(f"rmse: {rmse}")
+    trainer.save_model()
---
+++ Netflix/utils.py
@@ -0,0 +1,66 @@
+import os
+import pandas as pd
+import numpy as np
+
+class Movies:
+
+    ROOT = os.path.dirname(os.path.dirname(__file__))
+    def get_data(self):
+        """ get_data read all txt file and store them in one file """
+        if not os.path.isfile(os.path.join(self.ROOT,'raw_data','data.csv')): 
+            data = open(os.path.join(self.ROOT,'raw_data','data.csv'), mode='w')
+            row = list()
+            raw_datasets = ['combined_data_1.txt', 'combined_data_2.txt',
+                            'combined_data_3.txt', 'combined_data_4.txt']
+            path_list = []
+            for raw_dataset in raw_datasets:
+                file_path = os.path.join(self.ROOT, 'raw_data', raw_dataset)
+                path_list.append(file_path)
+            
+            for file in path_list:
+                    with open(file) as f:
+                        for line in f:
+                            del row[:]
+                            line = line.strip()
+                            if line.endswith(':'):
+                                movid_id = line.replace(':', '')
+                            else:
+                                row = [rows for rows in line.split(',')]
+                                row.insert(0, movid_id)
+                                data.write(','.join(row))
+                                data.write('\n')
+            data.close()
+        
+            # creating new dataframe from data.csv
+            df = pd.read_csv(os.path.join(self.ROOT, 'raw_data', 'data.csv'), sep=',', names=['movie_id','user','rating','date'])
+
+            # groupby 
+            df = df.groupby('movie_id')['rating'].agg(['mean','count']).reset_index()
+            
+            # merge with movie titles (see --> def movie_titles)
+            titles = self.movie_titles().merge(df, on='movie_id')
+            titles.to_csv(os.path.join(self.ROOT, 'raw_data', 'data.csv'), index=False)
+        
+        return pd.read_csv(os.path.join(self.ROOT, 'raw_data', 'data.csv'))
+
+
+    ## -----------------------------
+    
+    def movie_titles(self):
+        """ import movie titles and add to grouped data file """
+        df_title = pd.read_csv(os.path.join(self.ROOT, 'raw_data','movie_titles.csv'),
+                               header = None, names = ['movie_id', 'Year', 'Name', 'a', 'b' ,'c'])
+        df = df_title.drop(columns=['a', 'b', 'c'])
+        return df
+
+
+    ## -----------------------------
+    
+
+    
+    
+if __name__ == "__main__":
+    print(Movies().get_data().head())
+    print(Movies().movie_titles().head())
+    
+    
